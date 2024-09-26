f4cbf6936b1811904ba37cc3e822b3bcc84a36c5
yourbroF1zzy <fuzzt000@gmail.com>
2024-09-26 20:23:44 +0200

    Create stackbit.config.js

---
+++ .vscode/stackbit.config.js
@@ -0,0 +1,15 @@
+import { ContentfulContentSource } from '@stackbit/cms-contentful';
+
+export default {
+  contentSources: [
+    new ContentfulContentSource({
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      environment: process.env.CONTENTFUL_ENVIRONMENT,
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
+    }),
+  ],
+  modelExtensions: {
+    // Define your page models and field extensions here
+  },
+};
